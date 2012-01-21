13793c1ba0a15a357c8beacd884e7e809bbba91f
RepGenie <clayyearsley@yahoo.com>
2012-01-21 17:37:12 -0600

    Add fields. Update views on my own.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20120121223819
+# Schema version: 20120121231720
 #
 # Table name: articles
 #
@@ -9,6 +9,8 @@
 #  published_at :datetime
 #  created_at   :datetime
 #  updated_at   :datetime
+#  excerpt      :string(255)
+#  location     :string(255)
 #
 
 class Article < ActiveRecord::Base
